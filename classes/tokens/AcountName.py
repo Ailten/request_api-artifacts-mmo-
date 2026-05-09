6437dce0f47327b9aaddd754ce7a01cc668d568b
Ailten <faouzi26b@live.fr>
2026-05-09 21:00:52 +0200

    json

---
+++ classes/tokens/AcountName.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+from .AbstractToken import AbstractToken
+
+class AcountName(AbstractToken):
+    
+    def _readTokenEnv(self, path_file_env: str='.env'):
+        load_dotenv(dotenv_path=path_file_env)
+        self.token = os.getenv("ACOUNT_NAME")
